e1e2b74196681273a7856b349a2276a94c0bcfac
CielRipper <jaxontreadwell@gmail.com>
2026-05-10 23:21:58 -0500

    Added backend-connected dashboard with live database statistics

--- RazorPagesMovie/Pages/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Index.cshtml.cs
@@ -1,12 +1,41 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 
-namespace RazorPagesMovie.Pages;
-
-public class IndexModel : PageModel
+namespace RazorPagesMovie.Pages
 {
-    public void OnGet()
+    public class IndexModel : PageModel
     {
+        private readonly SocialHDXContext _context;
 
+        public IndexModel(SocialHDXContext context)
+        {
+            _context = context;
+        }
+
+        public int TotalStudents { get; set; }
+        public int TotalEvents { get; set; }
+        public int TotalPrescriptions { get; set; }
+        public int OpenCases { get; set; }
+
+        public IList<Prescription> RecentPrescriptions { get; set; } = new List<Prescription>();
+
+        public async Task OnGetAsync()
+        {
+            TotalStudents = await _context.Student.CountAsync();
+            TotalEvents = await _context.CampusEvent.CountAsync();
+            TotalPrescriptions = await _context.Prescription.CountAsync();
+            OpenCases = await _context.StudentCase
+                .CountAsync(c => c.CaseStatus == "Open" || c.CaseStatus == "In Progress");
+
+            RecentPrescriptions = await _context.Prescription
+                .Include(p => p.Student)
+                .Include(p => p.Prescriber)
+                .Include(p => p.CampusEvent)
+                .OrderByDescending(p => p.DateAssigned)
+                .Take(5)
+                .ToListAsync();
+        }
     }
 }
